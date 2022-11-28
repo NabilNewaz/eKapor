904955b95a99d1cb8fa5a8132601ef01f8d745e1
NabilNewaz <nabilnewaz@gmail.com>
2022-11-28 22:14:37 +0600

    reported option added

--- src/Pages/CategoryWiseProducts/CategoryWiseProducts.js
+++ src/Pages/CategoryWiseProducts/CategoryWiseProducts.js
@@ -9,10 +9,39 @@ import { IoCall } from "react-icons/io5";
 import { MdReport } from "react-icons/md";
 import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { AiFillWarning } from "react-icons/ai";
 
 const CategoryWiseProducts = () => {
     const { logOut } = useContext(AuthContext);
     const [productDetails, setPeoductDetails] = useState({});
+
+    let closeReportModalBtn = document.getElementById('report-modal-close');
+    const closeReportModal = () => {
+        closeReportModalBtn.click();
+    }
+
+    const handleReportProduct = (productID) => {
+        axios.patch(`http://localhost:5000/product-reported/${productID}`, {
+            isReported: true,
+        },
+            {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            }
+        )
+            .then(function () {
+                refetch();
+                closeReportModal();
+                toast.success('Product Reported')
+            })
+            .catch(function () {
+                toast.error('Something Went Wrong')
+            });
+    }
+
+
+
     let closeModalBtn = document.getElementById('modal-close');
     const closeMOdal = () => {
         closeModalBtn.click();
@@ -73,8 +102,6 @@ const CategoryWiseProducts = () => {
             .catch(function () {
                 toast.error('Something Went Wrong')
             });
-
-        console.log(bookedData);
         closeMOdal();
     }
 
@@ -109,6 +136,25 @@ const CategoryWiseProducts = () => {
             </div>
             <div className={(products.length <= 0) ? 'block' : 'hidden'}>
                 <p className='text-2xl font-semibold text-center text-slate-400 py-56'>No Products Found</p>
+            </div>
+            <div>
+                <input type="checkbox" id="productreportConferm-modal" className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box">
+                        <label id="report-modal-close" htmlFor="productreportConferm-modal" type="button" className="cursor-pointer absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            <span className="sr-only">Close modal</span>
+                        </label>
+                        <div className="p-6 text-center">
+                            <svg aria-hidden="true" className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" strokeLinejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to report this product?</h3>
+                            <button onClick={() => handleReportProduct(productDetails._id)} htmlFor="productreportConferm-modal" data-modal-toggle="popup-modal" type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                Yes, I'm sure
+                            </button>
+                            <label htmlFor="productreportConferm-modal" data-modal-toggle="popup-modal" type="button" className="cursor-pointer text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</label>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 <input type="checkbox" id="booknow-modal" className="modal-toggle" />
@@ -185,7 +231,10 @@ const CategoryWiseProducts = () => {
 
                                 </div>
                                 <div className='mt-1 flex justify-end'>
-                                    <button className="btn btn-xs btn-ghost btn-active flex items-center"><span className='text-lg'><MdReport /></span>Reported Item</button>
+                                    <span className={product.isReported ? 'hidden' : ''}><label onClick={() => setPeoductDetails(product)} htmlFor="productreportConferm-modal" className="btn btn-xs btn-ghost btn-active flex items-center"><span className='text-lg'><MdReport /></span>Report Item</label></span>
+
+                                    <span className={product.isReported ? ' ' : 'hidden'}><label className="btn btn-xs btn-error text-white btn-active flex items-center"><span className='text-lg'><AiFillWarning /></span>Reported</label></span>
+
                                 </div>
                                 <div className="card-actions justify-end">
                                     <label onClick={() => setPeoductDetails(product)} htmlFor="booknow-modal" className="btn btn-base-200 w-full">Book Now</label>
